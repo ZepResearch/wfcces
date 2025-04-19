d769e770f587ec1c326b87152c91fc679e8b1dbe
ShardulVanage <vsharadul@gmail.com>
2025-04-19 16:44:52 +0530

    temparary chatbot

--- src/app/layout.js
+++ src/app/layout.js
@@ -75,9 +75,26 @@ export default function RootLayout({ children }) {
         {children}
       <Footer/>
       <ToastProvider/>
-      <TawkToChat/>
+      {/* <TawkToChat/> */}
       <LineMessengerButton/>
        {/* <FloatingWhatsAppButton phoneNumber="8260080050" message="How can we assist you?"/> */}
+       <Script
+          id="chatbot-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.__ow = window.__ow || {};
+              window.__ow.organizationId = "6068a1a1-e755-4ddb-80e6-2d3677845184";
+              window.__ow.template_id = "8fec0e29-9657-4c22-833f-fa7a0b971881";
+              window.__ow.integration_name = "manual_settings";
+              window.__ow.product_name = "chatbot";   
+              ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))
+            `,
+          }}
+        />
+        <noscript>
+          You need to <a href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/" rel="noopener nofollow">enable JavaScript</a> in order to use the AI chatbot tool powered by <a href="https://www.chatbot.com/" rel="noopener nofollow" target="_blank">ChatBot</a>
+        </noscript>
       </body>
     </html>
   );
